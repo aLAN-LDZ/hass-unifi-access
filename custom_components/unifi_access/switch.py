cecaba8732dc2543961d7095ed99459212f468a7
SzakalLDZ <alan.j.com@gmail.com>
2024-07-09 22:23:39 +0200

    w

--- custom_components/unifi_access/switch.py
+++ custom_components/unifi_access/switch.py
@@ -14,8 +14,10 @@ async def async_setup_entry(
     """Set up the Unifi Access switches."""
     hub: UnifiAccessHub = hass.data[DOMAIN][entry.entry_id]
     switches = hub.get_switches()  # Assumes your hub has a method to get switches
-    if switches is None:
-        switches = []
+    if switches is None or not switches:
+        # Add a virtual switch if no switches are available
+        switches = [VirtualSwitch("Virtual Switch")]
+
     async_add_entities([UnifiAccessSwitch(hub, switch) for switch in switches], True)
 
 class UnifiAccessSwitch(SwitchEntity):
@@ -39,3 +41,34 @@ class UnifiAccessSwitch(SwitchEntity):
         await self._hub.turn_off_switch(self._switch.id)
         self._attr_is_on = False
         self.async_write_ha_state()
+
+class VirtualSwitch:
+    """Representation of a virtual switch."""
+
+    def __init__(self, name: str) -> None:
+        self._name = name
+        self._is_on = False
+        self._id = "virtual_switch"
+
+    @property
+    def name(self) -> str:
+        """Return the name of the switch."""
+        return self._name
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if switch is on."""
+        return self._is_on
+
+    @property
+    def id(self) -> str:
+        """Return the ID of the switch."""
+        return self._id
+
+    def turn_on(self) -> None:
+        """Turn the switch on."""
+        self._is_on = True
+
+    def turn_off(self) -> None:
+        """Turn the switch off."""
+        self._is_on = False
